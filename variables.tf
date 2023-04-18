e532606633342f05baede7a75887c8e05a503394
William Cummings <willjr20@gmail.com>
2023-04-18 07:21:49 -0400

    added files

--- variables.tf
+++ variables.tf
@@ -2,11 +2,14 @@
 variable "az_rg_name" {
   description = "The Name of the Resource Group"
   type        = string
+  default     = "rg-terraform-azurerm"
+
 }
 
 variable "az_rg_location" {
   description = "The Azure Region where the Resource Group should exist"
   type        = string
+  default     = "eastus"
 }
 
 variable "az_tags" {
